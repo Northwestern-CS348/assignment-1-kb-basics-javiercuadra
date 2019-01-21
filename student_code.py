8b695a09fae5fc06f034186213c27812d72c9da9
Javier Francisco Cuadra <32556567+javiercuadra@users.noreply.github.com>
2019-01-21 03:09:15 -0600

    Updated student_code.py
    Completed assert and ask

--- student_code.py
+++ student_code.py
@@ -19,10 +19,21 @@ class KnowledgeBase(object):
 
     def kb_assert(self, fact):
         """Assert a fact or rule into the KB
-
+		
         Args:
             fact (Fact or Rule): Fact or Rule we're asserting in the format produced by read.py
         """
+        #Checks to see if the fact is actually a fact
+        if(isinstance(fact, Fact)):
+        	#Checks if the fact is in the facts list
+        	if(fact not in self.facts):
+        		#Assuming the fact is not already in the fact list,
+        		#add the fact to the end of the fact list.
+        		self.facts.append(fact)
+
+
+        
+
         print("Asserting {!r}".format(fact))
         
     def kb_ask(self, fact):
@@ -34,4 +45,27 @@ class KnowledgeBase(object):
         Returns:
             ListOfBindings|False - ListOfBindings if result found, False otherwise
         """
+        bindingsArr=[]
+        answer=ListOfBindings()
+
+        #Checks to see if the fact is actually a fact
+        if(isinstance(fact, Fact)):
+        	#Check every fact in the KB's fact list
+        	for f in self.facts:
+        		#Check if the passed in fact matches the facts in the KB's fact list
+        		matched = match(fact.statement, f.statement)
+        		#Checks to see if the passed in fact did match the facts in the KB's fact list
+        		if (matched != False):
+        			#If it did match, pass the match to the answer array for list of bindings
+        			answer.add_bindings(matched)
+        		else:
+        			continue
+
+        
+
+        if (answer.list_of_bindings):
+        	return answer
+
+        return False
+
         print("Asking {!r}".format(fact))
